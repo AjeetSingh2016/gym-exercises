02ef9ce71dbe8562fdd0395dc2e136e12c99f818
Ajeet Singh <2016appsingh@gmail.com>
2022-08-26 02:46:35 +0530

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 
 
-import logo from '../assets/images/Logo-1.png'
+import logo from '../assets/images/Logo.png'
 
 
 const Footer = () => {
   return (
-    <Box mt="80px" bgcolor="primary.main" width="100%">
-      <Stack pb="20px" gap="40px" alignItems="center"px="40px" pt="24px" >
-        <img src={logo} alt="logo" width="200px" height="40px"/>
-        <Typography>My name is Ajeet Singh</Typography>
+    <Box mt="80px" bgcolor="transparent" width="100%">
+      <Stack pb="20px" gap="20px" alignItems="center"px="40px" pt="24px" >
+        <img src={logo} alt="logo" width="100px" height="80px"/>
+        <Typography fontWeight="bold" >Made by Ajeet Singh</Typography>
       </Stack>
       
     </Box>
