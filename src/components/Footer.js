0549e2e73ed8fbf32da72f4150ba5d849ae6b944
Ajeet Singh <2016appsingh@gmail.com>
2022-08-26 03:15:10 +0530

    master changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
     <Box mt="80px" bgcolor="transparent" width="100%">
       <Stack pb="20px" gap="20px" alignItems="center"px="40px" pt="24px" >
         <img src={logo} alt="logo" width="100px" height="80px"/>
-        <Typography fontWeight="bold" >Made by Ajeet Singh</Typography>
+        <Typography fontWeight="bold" >Made by Ajeet</Typography>
       </Stack>
       
     </Box>
